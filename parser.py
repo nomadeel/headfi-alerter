d56a1ff266e612d217994236d4810d2625695776
nomadeel <z5075672@student.unsw.edu.au>
2019-09-05 21:20:57 +1000

    Add headphones classifieds RSS feed parser
    The parser takes the headphones classifieds RSS feed and parses it. At
    the moment, it only prints each parsed classified.

---
+++ parser.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+from typing import Mapping
+import feedparser
+import re
+import urllib
+from bs4 import BeautifulSoup
+
+def check_ship_australia_or_worldwide(details: Mapping[str, str]) -> bool:
+    if "Anywhere" == details["Ship to"] or "Australia" == details["Ship to"]:
+        return True
+    return False
+
+def parse_details(url: str) -> Mapping[str, str]:
+    page = urllib.request.urlopen(item["link"])
+    soup = BeautifulSoup(page, "html.parser")
+    # Returns stuff like: 
+    # ['Type:', 'For Sale', 'Currency:', 'Euro', 'Price:', '750', 'Ship to:', 'Anywhere',
+    # 'Best offer:', 'Yes']
+    unparsed_info = list(soup.find_all(class_="pairsRows secondaryContent")[0].stripped_strings)
+    return dict(zip([x.rstrip(':') for x in unparsed_info[::2]], unparsed_info[1::2]))
+
+rss_url = "https://www.head-fi.org/forums/headphones-for-sale-trade.6550/index.rss"
+
+feed = feedparser.parse(rss_url)
+
+if feed["bozo"] == 1:
+    sys.exit("Malformed RSS feed! Exiting...")
+
+id_regex = re.compile(".*\.(\d+)/$")
+
+for item in feed["items"]:
+    details = parse_details(item["link"])
+    if not check_ship_australia_or_worldwide(details):
+        continue
+    print("Type: {}".format(details["Type"]))
+    print("Title: {}".format(item["title"]))
+    print("ID: {}".format(id_regex.match(item["guid"]).group(1)))
+    print("Price: {} {}".format(details["Price"], details["Currency"]))
